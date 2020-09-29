674f0ecd9b0a9f71ee8a829e6bd0641015fb41ab
junji9072 <flasusmaga@gmail.com>
2020-09-29 16:04:02 +0900

    [junji] 크롤러 초안 코드

--- crawler.py
+++ crawler.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 driver = webdriver.Chrome("본인 웹드라이버 저장 주소")
 
-driver.get('http: // ssullog.joins.com/speech/speechList?searchValue=% 27)
+driver.get('http: // ssullog.joins.com/speech/speechList?searchValue=% 27')
 
 html = driver.page_source
 soup = BeautifulSoup(html, "html.parser")
