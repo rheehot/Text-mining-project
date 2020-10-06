e70f51267664836854491e6fce9c8cc01adf9028
KIM SUYEON <s01090445778@gmail.com>
2020-10-06 21:14:07 +0900

    data_crawling

---
+++ crawling_data.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import time
+import sys
+
+#웹사이트 접속하기
+driver = webdriver.Chrome('C:/Users/user/chromedriver')
+driver.get('http://ssullog.joins.com/speech/speechList')
+
+#크롤러 생성
+text_file = [] #크롤링한 텍스트 데이터 저장
+def crawling():
+	count = 24713 #태그 번호
+
+	for i in range(50):
+		
+		#크롤링할 페이지 들어가기
+		link = '#speech_no_{} > div.box-header > div > a'.format(count)
+		sample = driver.find_element_by_css_selector(link)
+		sample.send_keys('\n')
+		time.sleep(5)
+
+		html = driver.page_source
+		soup = BeautifulSoup(html, 'html.parser')
+
+		#데이터를 크롤링해 저장하기
+		notices = soup.select('#pop_search > div.bd > div.box01')
+		text.file.append(notices)
+
+
+		#페이지 나가기
+		tag = '//*[@{}="btn_layer_speechall"]'.format('id')
+		closer = driver.find_element_by_xpath(tag)
+		closer.send_keys('\n')
+
+		count -= 1
+	return text_file
+		
+
+
+
